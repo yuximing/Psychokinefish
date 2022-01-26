70cb86682b2542180f54c74522a639bd1246fd2a
Brian Cho <b.cho@queensu.ca>
2022-01-26 01:14:34 -0500

    Remove Unused Var in RailScript

--- Assets/Scripts/RailScript.cs
+++ Assets/Scripts/RailScript.cs
@@ -6,7 +6,6 @@ public class RailScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public List<NodeScript> Nodes { get; protected set; }
-    public float s = 3;
     void Start()
     {
         Nodes = new List<NodeScript>(GetComponentsInChildren<NodeScript>());
