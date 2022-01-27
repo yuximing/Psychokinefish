80ce208de248c69c701927e978c43e28ff841c0d
Brian Cho <b.cho@queensu.ca>
2022-01-26 23:59:40 -0500

    Change Player's movement to use Rigidbody2D

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,13 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
     private int currentNodeIndex = 3;  // If player is between node n and n + 1, then currentNode = n
+    Rigidbody2D rbody;
+    int moveDirection = 0;             // normalized value where -1 = left, 1 = right
     void Start()
     {
         RailScript rail = railPrefab.GetComponent<RailScript>();
         transform.position = rail.Nodes[currentNodeIndex].transform.position;
+        rbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,11 +23,22 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A))
         {
-            MoveLeft(speed);
-        } else if (Input.GetKey(KeyCode.D))
-        {
-            MoveRight(speed);
+            moveDirection = -1;
         }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveDirection = 1;
+        } else
+        {
+            moveDirection = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (moveDirection == -1) MoveLeft(speed);
+        else if (moveDirection == 1) MoveRight(speed);
+
     }
 
     /*
@@ -49,9 +63,9 @@ public class PlayerController : MonoBehaviour
             Vector2 railLineProj = newCurNodePos - nextNodePos;
 
             // Get t val for Lerp
-            float t = railLineProj.normalized.sqrMagnitude * speed * Time.deltaTime / railLineProj.sqrMagnitude;
+            float t = railLineProj.normalized.sqrMagnitude * speed * Time.fixedDeltaTime / railLineProj.sqrMagnitude;
 
-            transform.position = Vector2.Lerp(nextNodePos, newCurNodePos, t);
+            rbody.MovePosition(Vector2.Lerp(nextNodePos, newCurNodePos, t));
         } 
         else // Player is between two nodes
         {
@@ -60,11 +74,12 @@ public class PlayerController : MonoBehaviour
             // Vector from nextNode to curNode;
             Vector2 railLineProj = currentNodePosition - nextNodePos;
             // Vector from nextNode to playerPos after moving
-            Vector2 playerProj = ((Vector2)transform.position + railLineProj.normalized * speed * Time.deltaTime) - nextNodePos;
+            Vector2 playerProj = ((Vector2)transform.position + railLineProj.normalized * speed * Time.fixedDeltaTime) - nextNodePos;
 
             // Vector projection to get the t value
             float t = Vector2.Dot(playerProj, railLineProj) / railLineProj.sqrMagnitude;
-            transform.position = Vector2.Lerp(nextNodePos, currentNodePosition, t);
+
+            rbody.MovePosition(Vector2.Lerp(nextNodePos, currentNodePosition, t));
         }
     }
 
@@ -88,8 +103,9 @@ public class PlayerController : MonoBehaviour
             Vector2 railLineProj = nextNodePos - newCurNodePos;
 
             // Get t value for Lerp
-            float t = railLineProj.normalized.sqrMagnitude * speed * Time.deltaTime / railLineProj.sqrMagnitude;
-            transform.position = Vector2.Lerp(newCurNodePos, nextNodePos, t);
+            float t = railLineProj.normalized.sqrMagnitude * speed * Time.fixedDeltaTime / railLineProj.sqrMagnitude;
+
+            rbody.MovePosition(Vector2.Lerp(newCurNodePos, nextNodePos, t));
         }
         else // Player is between two nodes
         {
@@ -97,11 +113,12 @@ public class PlayerController : MonoBehaviour
             
 
             Vector2 railLineProj = nextNodePosition - currentNodePosition;
-            Vector2 playerProj = ((Vector2)transform.position + railLineProj.normalized * speed * Time.deltaTime) - currentNodePosition;
+            Vector2 playerProj = ((Vector2)transform.position + railLineProj.normalized * speed * Time.fixedDeltaTime) - currentNodePosition;
 
             // Vector projection to get t value for Lerp
             float t = Vector2.Dot(playerProj, railLineProj) / railLineProj.sqrMagnitude;
-            transform.position = Vector2.Lerp(currentNodePosition, nextNodePosition, t);
+
+            rbody.MovePosition(Vector2.Lerp(currentNodePosition, nextNodePosition, t));
 
             // If player reached the next node, increment node index
             if (t >= 1.0f) ++currentNodeIndex;
