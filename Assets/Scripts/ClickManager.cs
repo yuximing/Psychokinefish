9dbd891e483dd4ba400105a4b5e1f93af253be1d
Brian Cho <b.cho@queensu.ca>
2022-02-01 16:45:17 -0500

    Update ClickManager to interact with ClickableObjects

--- Assets/Scripts/ClickManager.cs
+++ Assets/Scripts/ClickManager.cs
@@ -24,19 +24,29 @@ public class ClickManager : MonoBehaviour
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if (hit.collider != null)
+
+            if(hit.collider != null)
             {
-                //Debug.Log(hit.collider.gameObject.name);
-                if (hit.collider.CompareTag("Turret"))
+                var clickableScript = hit.collider.gameObject.GetComponent<ClickableGameObject>();
+                if (clickableScript != null)
                 {
-                    turret.GetComponent<TurretScript>().isMoving = false;
-                 
-                }
-                else if (hit.collider.CompareTag("Orb"))
-                {
-                    orb.GetComponent<OrbScript>().isMoving = false;
+                    clickableScript.ToggleActive();
                 }
             }
+
+            //if (hit.collider != null)
+            //{
+            //    //Debug.Log(hit.collider.gameObject.name);
+            //    if (hit.collider.CompareTag("Turret"))
+            //    {
+            //        turret.GetComponent<TurretScript>().isMoving = false;
+                 
+            //    }
+            //    else if (hit.collider.CompareTag("Orb"))
+            //    {
+            //        orb.GetComponent<OrbScript>().isMoving = false;
+            //    }
+            //}
         }
     }
 }
