178d6852711aaa1ce63d7dfe05f25ff02d648dad
Brian Cho <b.cho@queensu.ca>
2022-02-01 16:46:02 -0500

    Implement OrbScript by inheriting ClickableGameObject

--- Assets/Scripts/OrbScript.cs
+++ Assets/Scripts/OrbScript.cs
@@ -2,28 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrbScript : MonoBehaviour
+public class OrbScript : ClickableGameObject
 {
     public float speed = 5;
 
     private Rigidbody2D rb;
 
-    private bool toRight = true;
+    private Vector2 direction = Vector2.zero;
+    private float launchSpeed = 10.0f;
+    private bool hasLaunched = false;
 
-    public bool isMoving = true;
+    private bool toRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(-4.10f, 2, 0);
     }
 
     // Update is called once per frame
-    void Update()
+    public override void ToggleActive()
     {
-        if (isMoving) Move();
+        /* base.ToggleActive() would be what you would call to activate/deactivate the object
+         * and do something else with it after such as change a variable (speed, color, size, etc.)
+         * 
+         * In this case, we don't want to go from on to off so it is overriden to set
+         * isActive to true.
+         */
+        // base.ToggleActive();
+        //
+        // Do something here. For ex.
+        // speed = 3.0f;
 
+        isActive = true;
+    }
 
+    // MAKE SURE TO OVERRIDE USING 'override'!!!
+    protected override void OnActive()
+    {
+        if (Input.GetMouseButtonUp(0) && !hasLaunched)
+        {
+            Vector2 toMouseVec = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            if (toMouseVec.sqrMagnitude > 0.0f)
+            {
+                direction = toMouseVec.normalized;
+            } else
+            {
+                direction = Vector2.right;
+            }
+            hasLaunched = true;
+        }
+
+        if (!hasLaunched) Debug.DrawLine(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        if(hasLaunched) transform.position += (Vector3) direction * launchSpeed * Time.deltaTime;
+    }
+
+    protected override void OnInactive()
+    {
+        Move();
     }
 
     void Move()
