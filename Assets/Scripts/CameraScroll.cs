4b8baef0d5d5d0b128b5f9210c82d562c0c1c865
Brian Cho <b.cho@queensu.ca>
2022-02-02 17:30:37 -0500

    CreateCollisionBorders for Camera

--- Assets/Scripts/CameraScroll.cs
+++ Assets/Scripts/CameraScroll.cs
@@ -9,7 +9,7 @@ public class CameraScroll : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        CreateCollisionBorders();
     }
 
     // Update is called once per frame
@@ -19,5 +19,24 @@ public class CameraScroll : MonoBehaviour
         var cameraPos = Camera.main.gameObject.transform.position;
         cameraPos += speed * Time.deltaTime * (Vector3) direction;
         Camera.main.gameObject.transform.position = cameraPos;
+    }
+
+    void CreateCollisionBorders()
+    {
+        EdgeCollider2D edge = GetComponent<EdgeCollider2D>() == null ? gameObject.AddComponent<EdgeCollider2D>() : GetComponent<EdgeCollider2D>();
+        Vector2 bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+        Vector2 topRight = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight, Camera.main.nearClipPlane));
+        Vector2 topLeft = new Vector2(bottomLeft.x, topRight.y);
+        Vector2 bottomRight = new Vector2(topRight.x, bottomLeft.y);
+
+        Vector2[] edgePoints = new Vector2[5];
+
+        edgePoints[0] = bottomLeft;
+        edgePoints[1] = topLeft;
+        edgePoints[2] = topRight;
+        edgePoints[3] = bottomRight;
+        edgePoints[4] = bottomLeft;
+
+        edge.points = edgePoints;
     }
 }
