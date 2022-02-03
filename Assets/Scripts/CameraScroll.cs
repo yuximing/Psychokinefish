12c787e373517b80633115dabf423490b1bdc868
Brian Cho <b.cho@queensu.ca>
2022-02-02 20:53:26 -0500

    Add colliders to the screen edges

--- Assets/Scripts/CameraScroll.cs
+++ Assets/Scripts/CameraScroll.cs
@@ -6,10 +6,21 @@ public class CameraScroll : MonoBehaviour
 {
     float speed = 1.5f;
     Vector2 direction = Vector2.right;
-    // Start is called before the first frame update
+    BoxCollider2D[] borders = new BoxCollider2D[4];
+
     void Start()
     {
-        CreateCollisionBorders();
+        BoxCollider2D up = gameObject.AddComponent<BoxCollider2D>();
+        BoxCollider2D right = gameObject.AddComponent<BoxCollider2D>();
+        BoxCollider2D down = gameObject.AddComponent<BoxCollider2D>();
+        BoxCollider2D left = gameObject.AddComponent<BoxCollider2D>();
+
+        borders[0] = up;
+        borders[1] = right;
+        borders[2] = down;
+        borders[3] = left;
+
+        UpdateCollisionBorders();
     }
 
     // Update is called once per frame
@@ -19,24 +30,33 @@ public class CameraScroll : MonoBehaviour
         var cameraPos = Camera.main.gameObject.transform.position;
         cameraPos += speed * Time.deltaTime * (Vector3) direction;
         Camera.main.gameObject.transform.position = cameraPos;
+        // Camera.main.orthographicSize -= Time.deltaTime * 0.1f;
+        UpdateCollisionBorders();
     }
 
-    void CreateCollisionBorders()
+    void UpdateCollisionBorders()
     {
-        EdgeCollider2D edge = GetComponent<EdgeCollider2D>() == null ? gameObject.AddComponent<EdgeCollider2D>() : GetComponent<EdgeCollider2D>();
-        Vector2 bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
-        Vector2 topRight = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight, Camera.main.nearClipPlane));
-        Vector2 topLeft = new Vector2(bottomLeft.x, topRight.y);
-        Vector2 bottomRight = new Vector2(topRight.x, bottomLeft.y);
+        Vector2 cameraPos = Camera.main.transform.position;
 
-        Vector2[] edgePoints = new Vector2[5];
+        Vector2 bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.transform.position.z)) - (Vector3) cameraPos;
+        Vector2 topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)) - (Vector3)cameraPos;
+        Vector2 topLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, Camera.main.transform.position.z)) - (Vector3)cameraPos;
+        Vector2 bottomRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, Camera.main.transform.position.z)) - (Vector3)cameraPos;
 
-        edgePoints[0] = bottomLeft;
-        edgePoints[1] = topLeft;
-        edgePoints[2] = topRight;
-        edgePoints[3] = bottomRight;
-        edgePoints[4] = bottomLeft;
+        float height = topRight.y - bottomRight.y;
+        float width = topRight.x - topLeft.x;
 
-        edge.points = edgePoints;
+        borders[0].offset = new Vector2(0, topRight.y + 1);
+        borders[0].size = new Vector2(width, 2);
+
+        borders[1].offset = new Vector2(topRight.x + 1, 0);
+        borders[1].size = new Vector2(2, height);
+
+        borders[2].offset = new Vector2(0, bottomRight.y - 1);
+        borders[2].size = new Vector2(width, 2);
+
+        borders[3].offset = new Vector2(bottomLeft.x - 1, 0);
+        borders[3].size = new Vector2(2, height);
+
     }
 }
