5f554b4d1fca095a68b522e353ac8e318ebb75d6
Brian Cho <b.cho@queensu.ca>
2022-02-05 01:30:16 -0500

    Implement Chaser movement

--- Assets/Scripts/Enemies/ChaserController.cs
+++ Assets/Scripts/Enemies/ChaserController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class ChaserController : MonoBehaviour
 {
+    GameObject targetObject;
+
+    Rigidbody2D rbody;
+    public int hp = 5;
+    float maxSpeed = 5.0f;
+    float seekForce = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbody = GetComponent<Rigidbody2D>();
+        targetObject = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 target = targetObject.transform.position;
+        Seek(target);
+    }
+    void Seek(Vector2 target)
+    {
+        Vector2 position = transform.position;
+        Vector2 forceDir = target - position;
+        forceDir.Normalize();
+        forceDir *= seekForce;
+        rbody.AddForce(forceDir);
+        if(rbody.velocity.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            rbody.velocity = rbody.velocity.normalized * maxSpeed;
+        }
+        rbody.MoveRotation(Vector2.SignedAngle(Vector2.right, rbody.velocity));
     }
 }
