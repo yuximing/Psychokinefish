819a6669cca675af008a45071cb7bef7e9c97232
yuximing <93099324+yuximing@users.noreply.github.com>
2022-02-06 21:29:15 -0500

    closes #6

--- Assets/Scripts/ClickManager.cs
+++ Assets/Scripts/ClickManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ClickManager : MonoBehaviour
 {
+    [SerializeField]
+    private LayerMask layerMask;
+
     // Update is called once per frame
     void Update()
     {
@@ -12,7 +15,7 @@ public class ClickManager : MonoBehaviour
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero, 0, layerMask);
 
             if(hit.collider != null)
             {
