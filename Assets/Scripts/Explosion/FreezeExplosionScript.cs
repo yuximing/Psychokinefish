f1e862a26048845ab9347dfa734d905fd4775df6
yuximing <93099324+yuximing@users.noreply.github.com>
2022-02-09 23:59:25 -0500

    Add Freezer and FreezeExplosion prefabs

---
+++ Assets/Scripts/Explosion/FreezeExplosionScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeExplosionScript : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask layerMask;
+
+    Timer explosionTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosionTimer = new Timer(1.5f, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        explosionTimer.Tick();
+        Collider2D[] frozen = Freeze();
+        Debug.Log(frozen);
+        if (explosionTimer.IsReady())
+        {
+            UnFreeze(frozen);
+            Destroy(gameObject);
+        }
+    }
+
+    Collider2D[] Freeze()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, 1.0f, layerMask);
+
+        foreach (var hitCollider in hitColliders)
+        {
+            var rb = hitCollider.gameObject.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.bodyType = RigidbodyType2D.Static;
+            }
+        }
+        return hitColliders;
+
+    }
+
+    void UnFreeze(Collider2D[] hitColliders)
+    {
+        foreach (var hitCollider in hitColliders)
+        {
+            var rb = hitCollider.gameObject.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/FreezerScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezerScript : ClickableGameObject
+{
+    [SerializeField]
+    GameObject explosionPrefab;
+
+    private Vector2 direction = Vector2.zero;
+    private float launchSpeed = 10.0f;
+    private bool hasLaunched = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void ToggleActive()
+    {
+        isActive = true;
+    }
+
+    protected override void OnActive()
+    {
+        if (Input.GetMouseButtonUp(1) && !hasLaunched)
+        {
+            Vector2 toMouseVec = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            if (toMouseVec.sqrMagnitude > 0.0f)
+            {
+                direction = toMouseVec.normalized;
+            }
+            else
+            {
+                direction = Vector2.right;
+            }
+            hasLaunched = true;
+        }
+
+        if (!hasLaunched) Debug.DrawLine(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        if (hasLaunched) transform.position += (Vector3)direction * launchSpeed * Time.deltaTime;
+    }
+
+    protected override void OnInactive()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isActive && hasLaunched)
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
