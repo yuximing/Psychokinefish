86dfe0893b41286b086444fd05145d4527eec148
Brian Cho <b.cho@queensu.ca>
2022-02-10 22:42:49 -0500

    Update ChaserController

--- Assets/Scripts/Enemies/ChaserController.cs
+++ Assets/Scripts/Enemies/ChaserController.cs
@@ -8,19 +8,13 @@ public class ChaserController : MonoBehaviour
 
     Rigidbody2D rbody;
     public int hp = 5;
-    float maxSpeed = 5.0f;
-    float seekForce = 10.0f;
+    readonly float maxSpeed = 5.0f;
+    readonly float seekForce = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
         targetObject = GameObject.FindWithTag("Player");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void FixedUpdate()
