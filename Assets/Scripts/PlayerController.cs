658eb0a26fdf0541e6160cf8e91337c25d884552
Brian Cho <b.cho@queensu.ca>
2022-03-03 23:19:20 -0500

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -295,6 +295,7 @@ public class PlayerController : MonoBehaviour
         {
             yield return new WaitForSeconds(1.0f);
         }
+        yield return new WaitForSeconds(3.0f);
         CameraScroll.ToggleMove();
         MoveToNextLevel();
     }
