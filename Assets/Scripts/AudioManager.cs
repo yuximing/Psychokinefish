5a00d4cb178a9f2130b2a89a233d17bfb2df9a4b
Brian Cho <b.cho@queensu.ca>
2022-03-19 12:18:16 -0400

    Add AudioManager Prefab

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    Dictionary<AudioClip, float> audioLatencyMap;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioLatencyMap = new Dictionary<AudioClip, float>();
+    }
+
+    private void Update()
+    {
+        foreach(var key in audioLatencyMap.Keys.ToList())
+        {
+            audioLatencyMap[key] -= Time.deltaTime;
+            if (audioLatencyMap[key] < 0.0f) audioLatencyMap[key] = 0.0f;
+        }
+    }
+
+    public void PlayOneShot(AudioClip audioClip, float volume = 1.0f)
+    {
+        if (!audioLatencyMap.ContainsKey(audioClip))
+        {
+            audioLatencyMap[audioClip] = 0.0f;
+        }
+        volume *= 0.075f / (audioLatencyMap[audioClip] + 0.075f);
+        StartCoroutine(PlayOneShotCoroutine(audioClip, volume, audioLatencyMap[audioClip]));
+        audioLatencyMap[audioClip] += Random.Range(0.01f, 0.02f);
+        Debug.Log(audioLatencyMap[audioClip]);
+    }
+
+    IEnumerator PlayOneShotCoroutine(AudioClip audioClip, float volume, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        audioSource.PlayOneShot(audioClip,volume);
+    }
+}
