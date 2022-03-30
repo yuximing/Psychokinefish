5d525531ddf71270ae7ab9cfce50d874ee811d72
Brian Cho <b.cho@queensu.ca>
2022-03-30 00:23:43 -0400

    Update MainMenuManagerScript.cs

--- Assets/Scripts/MainMenuManagerScript.cs
+++ Assets/Scripts/MainMenuManagerScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuManagerScript : MonoBehaviour
@@ -9,22 +7,41 @@ public class MainMenuManagerScript : MonoBehaviour
 
     GameObject logo;
     GameObject creators;
+    GameObject instructions;
+
+    TurretScript turretScript;
 
     float timeElapsed = 0.0f;
+
+    string[] instructionTexts = 
+        { 
+        "Use A and D to move along the rail",
+        "Right click bubble objects to take control of them",
+        "Left click to use the object and destroy the box",
+        "Eat the food to finish the level",
+        "Good luck!"
+        };
+    int instructionIndex = 0;
+    bool[] instructionCheck = new bool[5];
     // Start is called before the first frame update
     void Start()
     {
         logo = canvas.transform.Find("Logo").gameObject;
         creators = canvas.transform.Find("Creators").gameObject;
+        instructions = canvas.transform.Find("Instructions").gameObject;
+
+        turretScript = gameObjectGroup.transform.Find("TurretSet").gameObject.transform.Find("Turret").GetComponent<TurretScript>();
 
         logo.SetActive(false);
         creators.SetActive(false);
+        instructions.SetActive(false);
         gameObjectGroup.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         timeElapsed += Time.deltaTime;
         switch ((int) timeElapsed)
         {
@@ -36,9 +53,23 @@ public class MainMenuManagerScript : MonoBehaviour
                 break;
             case 3:
                 gameObjectGroup.SetActive(true);
+                instructions.SetActive(true);
                 break;
             default:
                 break;
         }
+
+        if (gameObjectGroup.activeSelf)
+        {
+            if (Input.GetKey(KeyCode.D)) instructionCheck[0] = true;
+            if (turretScript.IsActive) instructionCheck[1] = true;
+            if (gameObjectGroup.transform.Find("Blockade") == null) instructionCheck[2] = true;
+            if (gameObjectGroup.transform.Find("Food") == null) instructionCheck[3] = true;
+
+            while (instructionCheck[instructionIndex]) ++instructionIndex;
+
+            if (instructions.activeSelf) instructions.GetComponent<UnityEngine.UI.Text>().text = instructionTexts[instructionIndex];
+        }
+
     }
 }
